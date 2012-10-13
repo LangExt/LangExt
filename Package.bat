c202032e166e8aad9857dab865bcff76bb75e92e
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2012-10-13 16:16:30 +0900

    fix build script

--- Package.bat
+++ Package.bat
@@ -3,7 +3,7 @@ rmdir /S /Q bin
 
 rmdir /S /Q LangExt.Compatibility.FSharp\obj
 mkdir specify4.0
-msbuild LangExt.Compatibility.FSharp\LangExt.Compatibility.FSharp.csproj /p:DotNet=4.0;Configuration=Relaese;Platform=AnyCPU /t:Rebuild
+msbuild LangExt.Compatibility.FSharp\LangExt.Compatibility.FSharp.fsproj /p:DotNet=4.0;Configuration=Relaese;Platform=AnyCPU /t:Rebuild
 rmdir specify4.0
 
 nuget pack Package.nuspec
