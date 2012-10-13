04d35ddd56488af340f938b28ba23debc375a5ee
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2012-10-13 15:49:08 +0900

    init

---
+++ Package.bat
@@ -0,0 +1,10 @@
+set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
+rmdir /S /Q bin
+
+rmdir /S /Q LangExt.Compatibility.FSharp\obj
+mkdir specify4.0
+msbuild LangExt.Compatibility.FSharp\LangExt.Compatibility.FSharp.csproj /p:DotNet=4.0;Configuration=Relaese;Platform=AnyCPU /t:Rebuild
+rmdir specify4.0
+
+nuget pack Package.nuspec
+move *.nupkg bin\
