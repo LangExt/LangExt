ed030729858dff06ff0c1a80c173e57a0946b0fa
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-05-06 01:58:06 +0900

    NuGetの情報を更新

--- Package.bat
+++ Package.bat
@@ -2,16 +2,8 @@ set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
 rmdir /S /Q bin
 
 rmdir /S /Q LangExt\obj
-mkdir specify4.0
-powershell tools/t4.ps1
-msbuild LangExt\LangExt.csproj /p:DotNet=4.0;Configuration=Relaese;Platform=AnyCPU /t:Rebuild
-rmdir specify4.0
+msbuild LangExt.sln /t:Rebuild /p:Configuration=Release
 
-rmdir /S /Q LangExt\obj
-mkdir specify3.5
-powershell tools/t4.ps1
-msbuild LangExt\LangExt.csproj /p:DotNet=3.5;Configuration=Relaese;Platform=AnyCPU /t:Rebuild
-rmdir specify3.5
-
-nuget pack Package.nuspec
+.nuget\nuget pack Package.nuspec
+mkdir bin
 move *.nupkg bin\
