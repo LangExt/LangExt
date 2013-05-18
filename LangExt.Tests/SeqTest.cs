db96ab1ba26ba386bcb070e6995fea36098d374a
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-05-18 22:31:55 +0900

    Seq/EmptySeq/OrderedSeqのToStringのテストを追加 refs #22
    Cachedはバグってるので後回し

--- LangExt.Tests/SeqTest.cs
+++ LangExt.Tests/SeqTest.cs
@@ -12,5 +12,37 @@ namespace LangExt.Tests
         {
             Assert.That(Seq.Empty<int>().IsEmpty(), Is.True);
         }
+
+        [TestCase(new int[0], "seq []")]
+        [TestCase(new[] { 1 }, "seq [1]")]
+        [TestCase(new[] { 1, 2 }, "seq [1, 2]")]
+        [TestCase(new[] { 1, 2, 3 }, "seq [1, 2, 3]")]
+        [TestCase(new[] { 1, 2, 3, 4 }, "seq [1, 2, 3, 4]")]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, "seq [1, 2, 3, 4, 5]")]
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6 }, "seq [1, 2, 3, 4, 5, ...]")]
+        public void ToString(int[] values, string expected)
+        {
+            var sut = Create.Seq(values);
+            Assert.That(sut.ToString(), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ToString_EmptySeq()
+        {
+            Assert.That(Seq.Empty<int>().ToString(), Is.EqualTo("seq []"));
+        }
+
+        [TestCase(new int[0], "seq []")]
+        [TestCase(new[] { 1 }, "seq [1]")]
+        [TestCase(new[] { 1, 2 }, "seq [1, 2]")]
+        [TestCase(new[] { 1, 2, 3 }, "seq [1, 2, 3]")]
+        [TestCase(new[] { 1, 2, 3, 4 }, "seq [1, 2, 3, 4]")]
+        [TestCase(new[] { 1, 2, 3, 4, 5 }, "seq [1, 2, 3, 4, 5]")]
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6 }, "seq [1, 2, 3, 4, 5, ...]")]
+        public void ToString_OrderedSeq(int[] values, string expected)
+        {
+            var sut = Create.Seq(values).SortBy(i => i);
+            Assert.That(sut.ToString(), Is.EqualTo(expected));
+        }
     }
 }
