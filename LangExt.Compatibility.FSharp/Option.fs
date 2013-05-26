7e8868b69ea2b4f750306210a2e8fd94d69822f9
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-05-26 16:31:05 +0900

    LangExtのOption用のアクティブパターンを追加

---
+++ LangExt.Compatibility.FSharp/Option.fs
@@ -0,0 +1,9 @@
+﻿namespace LangExt.Compatibility.FSharp
+
+open LangExt
+
+module Option =
+  let (|CsSome|CsNone|) (opt: Option<_>) =
+    opt.Match(
+      (fun v -> CsSome v),
+      (fun () -> CsNone))
