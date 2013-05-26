6002312f437157e7c0eb13c57363446c32596054
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-05-26 16:32:54 +0900

    Result用のアクティブパターンを追加

---
+++ LangExt.Compatibility.FSharp/Result.fs
@@ -0,0 +1,9 @@
+﻿namespace LangExt.Compatibility.FSharp
+
+open LangExt
+
+module Result =
+  let (|Success|Failure|) (result: Result<_, _>) =
+    result.Match(
+      (fun v -> Success v),
+      (fun v -> Failure v))
