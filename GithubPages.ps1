1ce023908140fe5dbfade9fe73311b2686161786
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-06-01 23:57:14 +0900

    GithubPages.ps1をより安全側に倒した

--- GithubPages.ps1
+++ GithubPages.ps1
@@ -1,4 +1,17 @@
-git clean -dfx
+$crntBranch = git symbolic-ref HEAD
+if ($crntBranch -ne 'refs/heads/master') {
+  echo 'please checkout master.'
+  exit 1
+}
+
+$cleanTargets = git clean -dnx
+if (-not [string]::IsNullOrEmpty($cleanTargets)) {
+  echo 'please clean workspace.'
+  echo $cleanTargets
+  exit 1
+}
+
+$id = git rev-parse master
 mkdir tmp
 cd doc
 .\gendoc.ps1
@@ -9,6 +22,7 @@ git checkout gh-pages
 rm *.html
 rm *.css
 mv tmp\* .
+rmdir tmp
 rmdir -Recurse -Force doc
 git add -A
-git commit --no-verify -m "update document."
+git commit --no-verify -m "update document.`n`nid: $id"
