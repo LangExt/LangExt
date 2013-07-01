19b0cfb5f3fc64cfa9ad1c457d0c347dc0afbeb3
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-07-01 22:00:11 +0900

    ドキュメントにナビゲーションを追加

--- doc/gendoc.ps1
+++ doc/gendoc.ps1
@@ -2,36 +2,50 @@
 ページ一覧
 ==========
 '@ | Out-File index.markdown -Encoding UTF8
-
+ 
 @'
 LangExtについて
 ---------------
 * [LangExtの概要](LangExt.html)
 * [ビルド方法](HowToBuild.html)
-
 '@ | Out-File index.markdown -Encoding UTF8 -Append
-
+ 
 @'
+ 
 設計選択について
 ----------------
 '@ | Out-File index.markdown -Encoding UTF8 -Append
-
+ 
 ls DesignChoice_*.markdown | %{
   $title = $(cat $_.Name -Encoding UTF8)[0]
   "* [$title]($([System.IO.Path]::ChangeExtension($_.Name, '.html')))"
 } | Out-File index.markdown -Encoding UTF8 -Append
-
+ 
 @'
-
+ 
 標準ライブラリとの比較
 ----------------------
 '@ | Out-File index.markdown -Encoding UTF8 -Append
-
+ 
 ls cmp_*.markdown | %{
   $title = $(cat $_.Name -Encoding UTF8)[0]
   "* [$title]($([System.IO.Path]::ChangeExtension($_.Name, '.html')))"
 } | Out-File index.markdown -Encoding UTF8 -Append
-
+ 
+@'
+ 
+外部リンク
+----------
+* [ソースコード(Github)](https://github.com/LangExt/LangExt)
+* [NuGetパッケージ](https://nuget.org/packages/LangExt/)
+* [Qiitaでの記事](http://qiita.com/tags/LangExt)
+'@ | Out-File index.markdown -Encoding UTF8 -Append
+ 
 ls *.markdown | %{
-  pandoc -s -o $([System.IO.Path]::ChangeExtension($_.Name, '.html')) --css=style.css $_.Name
+  $out = $([System.IO.Path]::ChangeExtension($_.Name, '.html'))
+  if ($_.Name -eq 'index.markdown') {
+    pandoc -s -o $out --css=style.css $_.Name
+  } else {
+    pandoc -s -o $out --css=style.css --include-before-body=header.html --include-after-body=footer.html $_.Name
+  }
 }
