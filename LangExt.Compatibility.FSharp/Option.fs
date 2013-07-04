560e8d8daf3ad7f667d97e9ce964400f3caa7c39
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-07-04 15:58:16 +0900

    ドキュメンテーションコメントを追加

--- LangExt.Compatibility.FSharp/Option.fs
+++ LangExt.Compatibility.FSharp/Option.fs
@@ -2,7 +2,12 @@
 
 open LangExt
 
+/// LangExtのOptionに対する操作を提供するモジュールです。
 module Option =
+  /// <summary>LangExtのOptionをmatch式で使えるようにします。</summary>
+  /// <returns>入力が値を保持したオブジェクトの場合CsSome、値を持っていないオブジェクトの場合CsNone</returns>
+  /// <remarks>このアクティブパターンは、OptionのMatchメソッドのビューです。</remarks>
+  [<CompiledName "OptionPattern">]
   let (|CsSome|CsNone|) (opt: Option<_>) =
     opt.Match(
       (fun v -> CsSome v),
--- LangExt.Compatibility.FSharp/Result.fs
+++ LangExt.Compatibility.FSharp/Result.fs
@@ -2,7 +2,12 @@
 
 open LangExt
 
+/// LangExtのResultに対する操作を提供するモジュールです。
 module Result =
+  /// <summary>LangExtのResultをmatch式で使えるようにします。</summary>
+  /// <returns>入力が成功を表すオブジェクトの場合Success、失敗を表すオブジェクトの場合Failure</returns>
+  /// <remarks>このアクティブパターンは、ResultのMatchメソッドのビューです。</remarks>
+  [<CompiledName "ResultPattern">]
   let (|Success|Failure|) (result: Result<_, _>) =
     result.Match(
       (fun v -> Success v),
