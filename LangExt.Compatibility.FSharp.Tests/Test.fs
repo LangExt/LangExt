29a7a6be05e73067314c3250008e7d1aad5335bf
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-07-04 16:57:11 +0900

    Resultのパターンマッチが使えることをテスト

--- LangExt.Compatibility.FSharp.Tests/Test.fs
+++ LangExt.Compatibility.FSharp.Tests/Test.fs
@@ -6,7 +6,7 @@ open LangExt.Compatibility.FSharp
 open NUnit.Framework
 
 [<Test>]
-let LangExtのOptionをパターンマッチで使える() =
+let LangExtのOptionをmatch式で使える() =
   match Option.Some(42) with
   | CsSome x -> Assert.AreEqual(42, x)
   | CsNone -> Assert.Fail()
@@ -14,3 +14,13 @@ let LangExtのOptionをパターンマッチで使える() =
   match Option.None with
   | CsSome _ -> Assert.Fail()
   | CsNone -> Assert.Pass()
+
+[<Test>]
+let LangExtのResultをmatch式で使える() =
+  match Result.Success("hoge") with
+  | Success str -> Assert.AreEqual("hoge", str)
+  | Failure _ -> Assert.Fail()
+
+  match Result.Failure("piyo") with
+  | Success _ -> Assert.Fail()
+  | Failure str -> Assert.AreEqual("piyo", str)
