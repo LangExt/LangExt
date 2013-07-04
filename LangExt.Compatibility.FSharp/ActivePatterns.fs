59146004a550f583c323bdc5fcb476d04ba9002e
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-07-04 17:54:52 +0900

    ActivePatternsのシグネチャファイルを追加

--- LangExt.Compatibility.FSharp/ActivePatterns.fs
+++ LangExt.Compatibility.FSharp/ActivePatterns.fs
@@ -2,21 +2,13 @@
 
 open LangExt
 
-/// LangExtの型をmatch式で使えるようにするためのアクティブパターンを提供するモジュールです。
-[<AutoOpen>]
 module ActivePatterns =
-  /// <summary>LangExtのOptionをmatch式で使えるようにします。</summary>
-  /// <returns>入力が値を保持したオブジェクトの場合CsSome、値を持っていないオブジェクトの場合CsNone</returns>
-  /// <remarks>このアクティブパターンは、OptionのMatchメソッドのビューです。</remarks>
   [<CompiledName "OptionPattern">]
   let (|CsSome|CsNone|) (opt: Option<_>) =
     opt.Match(
       (fun v -> CsSome v),
       (fun () -> CsNone))
 
-  /// <summary>LangExtのResultをmatch式で使えるようにします。</summary>
-  /// <returns>入力が成功を表すオブジェクトの場合Success、失敗を表すオブジェクトの場合Failure</returns>
-  /// <remarks>このアクティブパターンは、ResultのMatchメソッドのビューです。</remarks>
   [<CompiledName "ResultPattern">]
   let (|Success|Failure|) (result: Result<_, _>) =
     result.Match(
---
+++ LangExt.Compatibility.FSharp/ActivePatterns.fsi
@@ -0,0 +1,18 @@
+﻿namespace LangExt.Compatibility.FSharp
+
+open LangExt
+
+/// LangExtの型をmatch式で使えるようにするためのアクティブパターンを提供するモジュールです。
+[<AutoOpen>]
+module ActivePatterns =
+  /// <summary>LangExtのOptionをmatch式で使えるようにします。</summary>
+  /// <returns>入力が値を保持したオブジェクトの場合CsSome、値を持っていないオブジェクトの場合CsNone</returns>
+  /// <remarks>このアクティブパターンは、OptionのMatchメソッドのビューです。</remarks>
+  [<CompiledName "OptionPattern">]
+  val (|CsSome|CsNone|) : opt: Option<'a> -> Choice<'a, unit>
+
+  /// <summary>LangExtのResultをmatch式で使えるようにします。</summary>
+  /// <returns>入力が成功を表すオブジェクトの場合Success、失敗を表すオブジェクトの場合Failure</returns>
+  /// <remarks>このアクティブパターンは、ResultのMatchメソッドのビューです。</remarks>
+  [<CompiledName "ResultPattern">]
+  val (|Success|Failure|) : result: Result<'a, 'b> -> Choice<'a, 'b>
