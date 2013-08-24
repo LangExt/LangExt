77e639c73c1148d64883af34889c0cd0785b5ad8
otf <phoenixsingleton@gmail.com>
2013-08-25 05:11:09 +0900

    adds to apply to two options

--- LangExt.Tests/ApplicativeTest.cs
+++ LangExt.Tests/ApplicativeTest.cs
@@ -12,8 +12,17 @@ namespace LangExt.Tests
         [TestCase()]
         public void ApplyToTheOption()
         {
-            var applied = Apply.To(Option.Some(23)).By((i) => i.ToString());
+            var applied = Apply.To(Option.Some(23))
+                .By((i) => i.ToString());
             Assert.That(applied, Is.EqualTo(Option.Some("23")));
+        }
+
+        [TestCase()]
+        public void ApplyToTwoOption()
+        {
+            var applied = Apply.To(Option.Some(23), Option.Some("Something"))
+                .By((i, str) => i.ToString() + str);
+            Assert.That(applied, Is.EqualTo(Option.Some("23Something")));
         }
     }
 }
--- LangExt/Applicative.cs
+++ LangExt/Applicative.cs
@@ -7,14 +7,6 @@ namespace LangExt
 {
     public static class Apply
     {
-        public static class ApplicativeOption
-        {
-            internal static ApplicativeOption<TArg1> Create<TArg1>(Option<TArg1> m1)
-            {
-                return new ApplicativeOption<TArg1>(m1);
-            }
-        }
-
         public class ApplicativeOption<TArg1>
         {
             Option<TArg1> m1;
@@ -24,7 +16,6 @@ namespace LangExt
                 this.m1 = m1;
             }
 
-
             public Option<TResult> By<TResult>(Func<TArg1, TResult> f)
             {
                 return from arg1 in m1
@@ -32,9 +23,33 @@ namespace LangExt
             }
         }
 
+        public class ApplicativeOption<TArg1, TArg2>
+        {
+            Option<TArg1> m1;
+            Option<TArg2> m2;
+
+            internal ApplicativeOption(Option<TArg1> m1, Option<TArg2> m2)
+            {
+                this.m1 = m1;
+                this.m2 = m2;
+            }
+
+            public Option<TResult> By<TResult>(Func<TArg1, TArg2, TResult> f)
+            {
+                return from arg1 in m1
+                       from arg2 in m2
+                       select f(arg1, arg2);
+            }
+        }
+
         public static ApplicativeOption<TArg1> To<TArg1>(Option<TArg1> m1)
         {
-            return ApplicativeOption.Create(m1);
+            return new ApplicativeOption<TArg1>(m1);
+        }
+
+        public static ApplicativeOption<TArg1, TArg2> To<TArg1, TArg2>(Option<TArg1> m1, Option<TArg2> m2)
+        {
+            return new ApplicativeOption<TArg1, TArg2>(m1, m2);
         }
     }
 }
