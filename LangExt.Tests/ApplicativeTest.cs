444608e4ce338c13e58be31d73d1440d2c0a5931
otf <phoenixsingleton@gmail.com>
2013-08-25 05:02:38 +0900

    adds applicative w the option and testcode

---
+++ LangExt.Tests/ApplicativeTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LangExt.Tests
+{
+    [TestFixture]
+    public class ApplicativeTest
+    {
+        [TestCase()]
+        public void ApplyToTheOption()
+        {
+            var applied = Apply.To(Option.Some(23)).By((i) => i.ToString());
+            Assert.That(applied, Is.EqualTo(Option.Some("23")));
+        }
+    }
+}
---
+++ LangExt/Applicative.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LangExt
+{
+    public static class Apply
+    {
+        public static class ApplicativeOption
+        {
+            internal static ApplicativeOption<TArg1> Create<TArg1>(Option<TArg1> m1)
+            {
+                return new ApplicativeOption<TArg1>(m1);
+            }
+        }
+
+        public class ApplicativeOption<TArg1>
+        {
+            Option<TArg1> m1;
+
+            internal ApplicativeOption(Option<TArg1> m1) 
+            {
+                this.m1 = m1;
+            }
+
+
+            public Option<TResult> By<TResult>(Func<TArg1, TResult> f)
+            {
+                return from arg1 in m1
+                       select f(arg1);
+            }
+        }
+
+        public static ApplicativeOption<TArg1> To<TArg1>(Option<TArg1> m1)
+        {
+            return ApplicativeOption.Create(m1);
+        }
+    }
+}
