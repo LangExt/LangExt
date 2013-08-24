cf40ffca67c022ffbea68874365a53093bff5bcd
otf <phoenixsingleton@gmail.com>
2013-08-25 05:29:46 +0900

    add to apply to the seq

--- LangExt.Tests/ApplicativeTest.cs
+++ LangExt.Tests/ApplicativeTest.cs
@@ -13,7 +13,7 @@ namespace LangExt.Tests
         public void ApplyToTheOption()
         {
             var applied = Apply.To(Option.Some(23))
-                .By((i) => i.ToString());
+                .By(i => i.ToString());
             Assert.That(applied, Is.EqualTo(Option.Some("23")));
         }
 
@@ -23,6 +23,22 @@ namespace LangExt.Tests
             var applied = Apply.To(Option.Some(23), Option.Some("Something"))
                 .By((i, str) => i.ToString() + str);
             Assert.That(applied, Is.EqualTo(Option.Some("23Something")));
+        }
+
+        [TestCase()]
+        public void ApplyToTheSeq()
+        {
+            var applied = Apply.To(Seq.Init(3, Func.Id))
+                .By(i => i.ToString());
+            Assert.That(applied, Is.EqualTo(new []{ "0", "1", "2" }.ToSeq()));
+        }
+
+        [TestCase()]
+        public void ApplyToTwoSeq()
+        {
+            var applied = Apply.To(Seq.Init(2, Func.Id), Seq.Init(2, Func.Id))
+                .By((x1, x2) => x1 + x2); // [0 + 0; 0 + 1; 1 + 0; 1 + 1]
+            Assert.That(applied, Is.EqualTo(new[] { 0, 1, 1, 2 }.ToSeq()));
         }
     }
 }
--- LangExt/Applicative.cs
+++ LangExt/Applicative.cs
@@ -7,6 +7,7 @@ namespace LangExt
 {
     public static class Apply
     {
+        #region for the Option
         public class ApplicativeOption<TArg1>
         {
             Option<TArg1> m1;
@@ -51,5 +52,53 @@ namespace LangExt
         {
             return new ApplicativeOption<TArg1, TArg2>(m1, m2);
         }
+        #endregion
+
+        #region for the Seq
+        public class ApplicativeSeq<TArg1>
+        {
+            ISeq<TArg1> m1;
+
+            internal ApplicativeSeq(ISeq<TArg1> m1)
+            {
+                this.m1 = m1;
+            }
+
+            public ISeq<TResult> By<TResult>(Func<TArg1, TResult> f)
+            {
+                return from arg1 in m1
+                       select f(arg1);
+            }
+        }
+
+        public class ApplicativeSeq<TArg1, TArg2>
+        {
+            ISeq<TArg1> m1;
+            ISeq<TArg2> m2;
+
+            internal ApplicativeSeq(ISeq<TArg1> m1, ISeq<TArg2> m2)
+            {
+                this.m1 = m1;
+                this.m2 = m2;
+            }
+
+            public ISeq<TResult> By<TResult>(Func<TArg1, TArg2, TResult> f)
+            {
+                return from arg1 in m1
+                       from arg2 in m2
+                       select f(arg1, arg2);
+            }
+        }
+
+        public static ApplicativeSeq<TArg1> To<TArg1>(ISeq<TArg1> m1)
+        {
+            return new ApplicativeSeq<TArg1>(m1);
+        }
+
+        public static ApplicativeSeq<TArg1, TArg2> To<TArg1, TArg2>(ISeq<TArg1> m1, ISeq<TArg2> m2)
+        {
+            return new ApplicativeSeq<TArg1, TArg2>(m1, m2);
+        }
+        #endregion
     }
 }
