2c78a35e82fbd145814e0eb06cbf929ba34a2f41
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-08-30 16:31:08 +0900

    Rangeを実装 refs #40

---
+++ LangExt.Tests/RangeTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using LangExt.RangeUnit;
+using NUnit.Framework;
+
+namespace LangExt.Tests
+{
+    [TestFixture]
+    public class RangeTest
+    {
+        [Test]
+        public void 長さを指定してRangeを生成できる()
+        {
+            var range = Range.Create((10).ToLen());
+            Assert.That(range.From, Is.EqualTo(0));
+            Assert.That(range.To, Is.EqualTo(9));
+            Assert.That(range.Length, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void 始点と長さを指定してRangeを生成できる()
+        {
+            var range = Range.Create(3, (10).ToLen());
+            Assert.That(range.From, Is.EqualTo(3));
+            Assert.That(range.To, Is.EqualTo(12));
+            Assert.That(range.Length, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void 終了位置を指定してRangeを生成できる()
+        {
+            var range = Range.Create((10).ToIdx());
+            Assert.That(range.From, Is.EqualTo(0));
+            Assert.That(range.To, Is.EqualTo(10));
+            Assert.That(range.Length, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void 始点と終了位置を指定してRangeを生成できる()
+        {
+            var range = Range.Create(3, (10).ToIdx());
+            Assert.That(range.From, Is.EqualTo(3));
+            Assert.That(range.To, Is.EqualTo(10));
+            Assert.That(range.Length, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void FromToでRangeが生成できる()
+        {
+            var range = Range.FromTo(0, 10);
+            Assert.That(range, Is.EqualTo(Range.Create(0, (10).ToIdx())));
+        }
+
+        [Test]
+        public void FromToに同じ数値を渡すとそれのみを含むRangeが生成できる()
+        {
+            var range = Range.FromTo(10, 10);
+            Assert.That(range.From, Is.EqualTo(10));
+            Assert.That(range.To, Is.EqualTo(10));
+            Assert.That(range.Length, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Fromより小さいToを渡しても適切なRangeが生成できる()
+        {
+            var range = Range.FromTo(10, 5);
+            Assert.That(range.From, Is.EqualTo(10));
+            Assert.That(range.To, Is.EqualTo(5));
+            Assert.That(range.Length, Is.EqualTo(6));
+        }
+
+        [TestCase(0, 5, new[] { 0, 1, 2, 3, 4, 5 })]
+        [TestCase(1, 5, new[] { 1, 2, 3, 4, 5 })]
+        [TestCase(5, 0, new[] { 5, 4, 3, 2, 1, 0 })]
+        [TestCase(5, 5, new[] { 5 })]
+        // どうあがいても空のRangeは作れません
+        [TestCase(0, 0, new[] { 0 })]
+        [TestCase(0, 1, new[] { 0, 1 })]
+        [TestCase(0, -1, new[] { 0, -1 })]
+        [TestCase(1, 0, new[] { 1, 0 })]
+        [TestCase(-1, 0, new[] { -1, 0 })]
+        public void RangeをSeqに変換できる(int from, int to, int[] expected)
+        {
+            var range = Range.FromTo(from, to);
+            Assert.That(range.ToSeq(), Is.EqualTo(Seq.Create(expected)));
+        }
+    }
+}
---
+++ LangExt/Range.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace LangExt
+{
+    namespace RangeUnit
+    {
+        public sealed class Length : IUnit { Length() { } }
+        public sealed class Index : IUnit { Index() { } }
+
+        public static class Int
+        {
+            public static IntWithUnit<Length> ToLen(this int self) { return new IntWithUnit<Length>(self); }
+            public static IntWithUnit<Index> ToIdx(this int self) { return new IntWithUnit<Index>(self); }
+        }
+    }
+
+    partial class Create
+    {
+        public static Range Range(IntWithUnit<RangeUnit.Length> len)
+        {
+            return new Range(len.Value);
+        }
+
+        public static Range Range(int from, IntWithUnit<RangeUnit.Length> len)
+        {
+            return new Range(from, len.Value);
+        }
+
+        public static Range Range(IntWithUnit<RangeUnit.Index> to)
+        {
+            return new Range(0, to.Value + 1);
+        }
+
+        public static Range Range(int from, IntWithUnit<RangeUnit.Index> to)
+        {
+            return new Range(from, to.Value - from + 1);
+        }
+    }
+
+    public struct Range : IEquatable<Range>
+    {
+        public readonly int From;
+        public readonly int To;
+        public int Length
+        {
+            get
+            {
+                if (From <= To) return To - From + 1;
+                return From - To + 1;
+            }
+        }
+
+        Range(int from, int to, bool canNegLen)
+        {
+            this.From = from;
+            this.To = to;
+            if (!canNegLen && this.Length < 0)
+                throw new ArgumentOutOfRangeException("from, to", string.Format(Properties.Resources.ExMsgTooSmall, "(to - from + 1)", 0, this.Length));
+        }
+
+        public Range(int from, int len) : this(from, from + len - 1, false) { }
+
+        public Range(int len) : this(0, len - 1, false) { }
+
+        public static Range Create(IntWithUnit<RangeUnit.Length> len)
+        {
+            return new Range(len.Value);
+        }
+
+        public static Range Create(int from, IntWithUnit<RangeUnit.Length> len)
+        {
+            return new Range(from, len.Value);
+        }
+
+        public static Range Create(IntWithUnit<RangeUnit.Index> to)
+        {
+            return new Range(0, to.Value + 1);
+        }
+
+        public static Range Create(int from, IntWithUnit<RangeUnit.Index> to)
+        {
+            return new Range(from, to.Value - from + 1);
+        }
+
+        public static Range FromTo(int from, int to)
+        {
+            return new Range(from, to, true);
+        }
+
+        public ISeq<int> ToSeq()
+        {
+            var from = this.From;
+            if (from <= this.To)
+                return Seq.Init(this.Length, i => from + i);
+            return Seq.Init(this.Length, i => from - i);
+        }
+
+        public bool Equals(Range other)
+        {
+            return this.From == other.From && this.Length == other.Length;
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Range))
+                return false;
+            return this.Equals((Range)obj);
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode()
+        {
+            return 31 ^ this.From.GetHashCode() ^ this.To.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Range(From={0}, To={1}, Length={2})", this.From, this.To, this.Length);
+        }
+    }
+}
