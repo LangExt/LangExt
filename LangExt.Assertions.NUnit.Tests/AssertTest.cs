cb98c0fe77ec8a4f16755c0aa0c56ecf6c308a6e
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2014-04-16 11:46:29 +0900

    型指定されたEqualToを実装 refs #57

---
+++ LangExt.Assertions.NUnit.Tests/AssertTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using LangExt;
+
+using NTest = global::NUnit.Framework.TestAttribute;
+using NAssert = global::NUnit.Framework.Assert;
+
+namespace LangExt.Assertions.NUnit.Tests
+{
+    [global::NUnit.Framework.TestFixture]
+    public class AssertTest
+    {
+        void TestSuccess(Action assertion)
+        {
+            try
+            {
+                assertion();
+            }
+            catch (Exception e)
+            {
+                NAssert.Fail("成功すべきアサーションが失敗:\n" + e.Message + "\n" + e.StackTrace);
+            }
+        }
+
+        void TestFailure(Action assertion)
+        {
+            try
+            {
+                assertion();
+                NAssert.Fail("失敗すべきアサーションが成功");
+            }
+            catch (global::NUnit.Framework.AssertionException)
+            {
+                // 成功
+            }
+            catch (Exception e)
+            {
+                NAssert.Fail("アサーションの失敗以外の原因で失敗:\n" + e.Message + "\n" + e.StackTrace);
+            }
+        }
+
+        [NTest]
+        public void 型指定されたEqualToが使える()
+        {
+            TestSuccess(() => Assert.That(42, Is.EqualTo(42)));
+            TestFailure(() => Assert.That(42, Is.EqualTo(0)));
+        }
+
+        [NTest]
+        public void 型指定されたEqualToのNotが使える()
+        {
+            TestSuccess(() => Assert.That(42, Is.Not.EqualTo(0)));
+            TestFailure(() => Assert.That(42, Is.Not.EqualTo(42)));
+        }
+    }
+}
---
+++ LangExt.Assertions.NUnit/Assert.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using LangExt;
+using NUnit.Framework.Constraints;
+
+using NAssert = NUnit.Framework.Assert;
+using NIs = NUnit.Framework.Is;
+
+namespace LangExt.Assertions.NUnit
+{
+    public static class Assert
+    {
+        public static void That<T>(T actual, IAssertionExpression<T> expression)
+        {
+            NAssert.That(actual, expression.ToNUnitConstraint());
+        }
+    }
+
+    public interface IAssertionExpression<T>
+    {
+        IResolveConstraint ToNUnitConstraint();
+    }
+
+    internal sealed class AssertionExpression<T> : IAssertionExpression<T>
+    {
+        readonly IResolveConstraint constraint;
+
+        public AssertionExpression(IResolveConstraint constraint)
+        {
+            this.constraint = constraint;
+        }
+
+        public IResolveConstraint ToNUnitConstraint()
+        {
+            return this.constraint;
+        }
+    }
+
+    public static class Is
+    {
+        public static IAssertionExpression<T> EqualTo<T>(T expected)
+        {
+            return new AssertionExpression<T>(NIs.EqualTo(expected));
+        }
+
+        public static NotExpression Not
+        {
+            get { return NotExpression.Instance; }
+        }
+    }
+
+    public sealed class NotExpression
+    {
+        internal static readonly NotExpression Instance = new NotExpression();
+
+        public IAssertionExpression<T> EqualTo<T>(T expected)
+        {
+            return new AssertionExpression<T>(NIs.Not.EqualTo(expected));
+        }
+    }
+}
